f2237808fbac5835edbc68dd49f5e6b18f6cc4ce
Thoyaz <thoyazthoyazpc@gmail.com>
2026-07-01 10:23:23 +0530

    create ec2 and sg with TF

---
+++ ec2/ec2.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "tf-init-server" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.tf-init-sg.id]
+
+  tags = {
+    Name    = "Terraform Init Server"
+    Env     = "Dev"
+    Project = "roboshop"
+  }
+}
+
+resource "aws_security_group" "tf-init-sg" {
+  name        = "allow all traffic"
+  description = "allow all traffic terraform"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow all traffic TF"
+  }
+}
